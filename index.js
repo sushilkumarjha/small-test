6ef1ebacabd246c344e90ca3e61f7697ebb6d0f9
sushil jha <sunilkumarjha412@gmail.com>
2023-03-18 22:45:31 +0530

    wip

---
+++ index.js
@@ -0,0 +1,18 @@
+var fs=require("fs");
+// fs.writeFileSync("abc.txt","this is my radha");
+// var os=require("os");
+// var cpu=os.arch();
+// console.log(cpu);
+// console.log(os.cpus());
+
+for(let i=1;i<10;i++){
+let toMakeDir=`lecture ${i}`;
+// fs.mkdirSync(toMakeDir); 
+// fs.writeFileSync(toMakeDir + "//"+ "abc",`this is my ${toMakeDir} first lecture`/)
+// fs.unlinkSync(toMakeDir +"//"+ "abc");
+// fs.rmdirSync(toMakeDir);
+
+
+
+}
+fs.readFileSync("abc.txt");
